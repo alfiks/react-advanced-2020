4cbd04a4485efe9a53850900409c9a7aaf86e7c5
alfiks <alberts.alksnitis@gmail.com>
2021-02-25 17:19:43 +0000

    useState with array
    use .map to get data from array. Arrays must have ID..
    onClick must be a arrow function to run on click
    use .filter to filter out data from array, remove button etc

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { data } from '../../../data';
+import { data } from '../../../data'; // data must have id
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+  const [people, setPeople] = React.useState(data) // same as import {useState}
+  
+  const removeItem = (id) => {
+    let newPeople = people.filter((person)=> person.id !== id)
+    setPeople(newPeople) 
+  } 
+  // fragment
+  return (
+  <>
+  {people.map((person) => {
+    const {id,name} = person;
+    return (    
+    <div key={id} className="item">
+    <h4>{name}</h4>
+    <button onClick={() => removeItem(id)}>
+      remove
+    </button>
+    </div>
+    )
+  })}
+  <button className="btn" onClick={() => setPeople([])}>
+    clear items
+  </button>
+  </>
+  )
+
+}
 
 export default UseStateArray;
