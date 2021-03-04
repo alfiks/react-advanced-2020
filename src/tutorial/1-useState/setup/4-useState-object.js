9439e93e6867e7911146e1382c1f4701774909c2
alfiks <alberts.alksnitis@gmail.com>
2021-03-04 12:58:29 +0000

    useState
    For objects use spread operator ...  in onClick function. State values can be set as const. Remeber to change {person.name} to {name } if setting the const

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+const [person, setPerson] = useState ({name:"Peter", age: 34, message:"Random Message"})
+// const changeMessage = () => {setPerson({...person, message: "hello world"})}
 
+const [name, setName] = useState("peter");
+const [age, setAge] = useState(24);
+const[message, setMessage] = useState ("Diff");
+
+const changeMessage = () => {
+  setMessage ('hi')
+}
+
+  return <>
+  <h3>{name}</h3>
+    <h3>{age}</h3>
+  <h3>{message}</h3>
+  <button className="btn" onClick={changeMessage}>Click</button>
+
+  </>
+
+};
 export default UseStateObject;
