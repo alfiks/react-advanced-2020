a523fdda22e5191a612b7f9e19c1f03cfa9582d9
alfiks <alberts.alksnitis@gmail.com>
2021-03-13 14:45:54 +0000

    Use if statement inside useEffect
    useEffect(()=>{
      if (value > 1){
          document.title = `New Messages(${value})`
      }

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,17 +3,23 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  const [value, setValue] = useState(0)
-  useEffect(()=>{
+const [value, setValue] = useState(0)
+
+useEffect(()=>{
+
   console.log('call UseEffect');
-  document.title = `New Messages(${value})`
+  if (value > 1){
+      document.title = `New Messages(${value})`
+
+  }
 })
+
 console.log("render component")
+
 return <>
 <h1>{value}</h1>
 <button className="btn" onClick={() => setValue(value + 1)}>Click</button>
 </>
-  return <h2>useEffect Basics</h2>;
 };
 
 export default UseEffectBasics;
