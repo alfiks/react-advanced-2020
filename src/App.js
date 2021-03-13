12878085dd6996e8f82c5f202c8ca1ba41e7829e
alfiks <alberts.alksnitis@gmail.com>
2021-03-13 14:31:50 +0000

    Update App.js
    use effect run after render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Final from "./tutorial/1-useState/final/2-useState-basics"
+import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics"
 
   
 
 function App() {
   return (
     <div className="container">
-     <Final/>
+     <Setup/>
     </div>
   )
 }
