9f1e73683a7295f56c9488f23edfd05a7b36add6
alfiks <alberts.alksnitis@gmail.com>
2021-03-24 15:27:54 +0000

    Reducer
    Reducer helps to create a cleaner code and add consistency.

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,39 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const reducer = (state,action) => {
+    console.log(state,action)
+    return state;
+  }
+  const defaultState = {
+    people:[],
+    isModalOpen: false,
+    modalContent: ""
+  }
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer,defaultState)
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(name){
+      dispatch({type:"TESTING"})
+    }
+    else{
+    }
+  }
+  return <>
+  {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+  <form onSubmit={handleSubmit} className="form">
+    <div>
+      <input type="text" value={name} onChange={(e)=> setName(e.target.value)}></input>
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+  {state.people.map((person)=>{
+    return <div key={person.id}>
+<h4>{person.name}</h4>
+    </div>
+  })}
+  </>;
 };
 
 export default Index;
